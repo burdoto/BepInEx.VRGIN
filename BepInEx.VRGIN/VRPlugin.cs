e96cd9794a16ab6501c9f9991caa8c52033a912a
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-26 12:11:28 +0200

    Initial commit

---
+++ BepInEx.VRGIN/VRPlugin.cs
@@ -0,0 +1,8 @@
+﻿namespace BepInEx.VRGIN
+{
+    [BepInPlugin("BepInEx.VRGIN", "BepInEx VRGIN Loader", Version)]
+    public class VRPlugin
+    {
+        public const string Version = "0.0.1";
+    }
+}
