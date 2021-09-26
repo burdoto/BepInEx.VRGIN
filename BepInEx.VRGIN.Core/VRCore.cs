bbd059b79233b30bb7fc946892f3b8aaf7c9582b
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-26 12:41:06 +0200

    add core

---
+++ BepInEx.VRGIN.Core/VRCore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using VRGIN.Core;
+
+namespace BepInEx.VRGIN.Core
+{
+    public static class VRCore
+    {
+        public static Type VrContextType = typeof(IVRManagerContext);
+    }
+}
--- BepInEx.VRGIN/VRPlugin.cs
+++ BepInEx.VRGIN/VRPlugin.cs
@@ -7,9 +7,14 @@ using Debug = UnityEngine.Debug;
 namespace BepInEx.VRGIN
 {
     [BepInPlugin("BepInEx.VRGIN", "BepInEx VRGIN Loader", Version)]
-    [BepInDependency("BepInEx.VRGIN.Context")] // game-defining context module
+    [BepInDependency("BepInEx.VRGIN.Context")] // per-game-defined context module
     public class VRPlugin : BaseUnityPlugin
     {
         public const string Version = "0.0.1";
+
+        private void Awake()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
