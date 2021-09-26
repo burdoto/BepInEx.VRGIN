1159297bc973694c6cf6346417bee3996b560398
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-26 12:50:06 +0200

    e

--- BepInEx.VRGIN.Core/VRCore.cs
+++ BepInEx.VRGIN.Core/VRCore.cs
@@ -3,8 +3,13 @@ using VRGIN.Core;
 
 namespace BepInEx.VRGIN.Core
 {
+    [BepInPlugin(ModGuid_Core, "BepInEx VRGIN Mapping Provider", Version)]
     public static class VRCore
     {
+        public const string Version = "0.0.1";
+        public const string ModGuid = "BepInEx.VRGIN";
+        public const string ModGuid_Core = "BepInEx.VRGIN.Core";
+        public const string ModGuid_Context = "BepInEx.VRGIN.Context";
         public static Type VrContextType = typeof(IVRManagerContext);
     }
 }
--- BepInEx.VRGIN/VRPlugin.cs
+++ BepInEx.VRGIN/VRPlugin.cs
@@ -1,20 +1,60 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml.Serialization;
 using BepInEx;
+using BepInEx.VRGIN.Core;
 using HarmonyLib;
 using UnityEngine;
+using VRGIN.Core;
 using Debug = UnityEngine.Debug;
 
 namespace BepInEx.VRGIN
 {
-    [BepInPlugin("BepInEx.VRGIN", "BepInEx VRGIN Loader", Version)]
-    [BepInDependency("BepInEx.VRGIN.Context")] // per-game-defined context module
+    [BepInPlugin(VRCore.ModGuid, "BepInEx VRGIN Loader", VRCore.Version)]
+    [BepInDependency(VRCore.ModGuid_Core)] // context provider core module
+    [BepInDependency(VRCore.ModGuid_Context)] // per-game-defined context module
     public class VRPlugin : BaseUnityPlugin
     {
-        public const string Version = "0.0.1";
-
         private void Awake()
         {
-            throw new NotImplementedException();
+        }
+        
+        private IVRManagerContext CreateContext(string path) {
+            var serializer = new XmlSerializer(VRCore.VrContextType);
+
+            if(File.Exists(path))
+            {
+                // Attempt to load XML
+                using (var file = File.OpenRead(path))
+                {
+                    try
+                    {
+                        return serializer.Deserialize(file) as IVRManagerContext;
+                    }
+                    catch (Exception e)
+                    {
+                        VRLog.Error("Failed to deserialize {0} -- using default", path);
+                    }
+                }
+            }
+
+            // Create and save file
+            var context = VRCore.VrContextType.GetConstructor(Type.EmptyTypes)
+                ?.Invoke(Array.Empty<object>()) as IVRManagerContext;
+            try
+            {
+                using (var file = new StreamWriter(path))
+                {
+                    file.BaseStream.SetLength(0);
+                    serializer.Serialize(file, context);
+                }
+            } catch(Exception e)
+            {
+                VRLog.Error("Failed to write {0}", path);
+            }
+
+            return context;
         }
     }
 }
