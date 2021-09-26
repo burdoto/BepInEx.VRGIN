ae61c2e83004f32382a7b6ed95739e8382d07f5c
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-09-26 12:14:59 +0200

    base unity plugin

--- BepInEx.VRGIN/VRPlugin.cs
+++ BepInEx.VRGIN/VRPlugin.cs
@@ -1,7 +1,13 @@
-﻿namespace BepInEx.VRGIN
+﻿using System;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace BepInEx.VRGIN
 {
     [BepInPlugin("BepInEx.VRGIN", "BepInEx VRGIN Loader", Version)]
-    public class VRPlugin
+    public class VRPlugin : BaseUnityPlugin
     {
         public const string Version = "0.0.1";
     }
